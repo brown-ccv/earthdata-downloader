6d7dfa20d304107a76b0a5e59606e79c23d2fdba
John Gerrard Holland <john_holland1@brown.edu>
2026-05-07 19:37:37 +0000

    add support for antarctic landmasks from SCAR

--- tests/test_load.py
+++ tests/test_load.py
@@ -37,15 +37,31 @@ ExampleDataSetWeddellSea = DataSet(
     ts=1683675557694,
 )
 
+ExampleDataSetAntarctica = DataSet(
+    datetime="2025-12-09T00:00:00Z",
+    wrap="day",
+    satellite=Satellite.terra,
+    kind=ImageType.landmask,
+    scale=100000,
+    bbox=(
+        -5000000,
+        -5000000,
+        5000000,
+        5000000,
+    ),
+    crs="EPSG:3031",
+    ts=1683675557694,
+)
+
 
 @pytest.mark.slow
 @pytest.mark.parametrize("satellite", Satellite)
 @pytest.mark.parametrize("kind", ImageType)
-@pytest.mark.parametrize("dataset", [ExampleDataSetBeaufortSea, ExampleDataSetWeddellSea], ids=["beaufort-sea", "weddell-sea"])
+@pytest.mark.parametrize("dataset", [ExampleDataSetBeaufortSea, ExampleDataSetWeddellSea, ExampleDataSetAntarctica], ids=["beaufort-sea", "weddell-sea", "antarctica"])
 def test_load_runs_without_crashing_for_different_parameters(kind, satellite, dataset):
     load(kind=kind,
          satellite=satellite,
-         scale=100000,
+         scale=dataset.scale,
          datetime=dataset.datetime,
          wrap=dataset.wrap,
          bbox=dataset.bbox,
